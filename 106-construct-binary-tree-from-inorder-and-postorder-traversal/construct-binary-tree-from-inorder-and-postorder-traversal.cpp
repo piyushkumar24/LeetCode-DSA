a08588ded88c84b982bdf5b61cc06808d2dcc18b
PIYUSH KUMAR <93852522+piyushkumar24@users.noreply.github.com>
2024-02-18 22:22:06 +0530

    Time: 28 ms (23.41%) | Memory: 222.1 MB (5.56%) - LeetSync

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    void createmapping(vector<int> inorder,map<int,int>& nodetoindex,int n){
+        for(int i=0;i<n;i++){
+            nodetoindex[inorder[i]]=i;
+        }
+    }
+    TreeNode* solve(vector<int> in,vector<int> post,int &index, int inorderstart,int inorderend,map<int,int>& nodetoindex,int n){
+        if(index<0 || inorderstart > inorderend) return NULL;
+
+        int element=post[index];
+        TreeNode* root=new TreeNode(element);
+        int position=nodetoindex[element];
+        index-=1;
+
+        
+        root->right=solve(in,post,index,position+1,inorderend,nodetoindex,n);
+        root->left=solve(in,post,index,inorderstart,position-1,nodetoindex,n);
+
+        return root;
+    }
+
+
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int n=inorder.size();
+        int postorderindex=n-1;
+        
+        map<int,int> nodetoindex;
+        createmapping(inorder,nodetoindex,n);
+        TreeNode* ans=solve(inorder,postorder,postorderindex,0,n-1,nodetoindex,n);
+        return ans;
+    }
+};
