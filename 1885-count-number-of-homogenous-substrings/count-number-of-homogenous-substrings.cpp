9b891a8eceff6af82d92a9f30255651d1378e30a
PIYUSH KUMAR <93852522+piyushkumar24@users.noreply.github.com>
2024-04-12 22:40:17 +0530

    Time: 28 ms (46.28%) | Memory: 13.3 MB (50.64%) - LeetSync

---
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.cpp
@@ -0,0 +1,42 @@
+// APPROACH-1
+
+// class Solution {
+// public:
+//     int countHomogenous(string s) {
+//         vector<int> Map(s.size(),1);
+//         int sum = 1;
+//         for(int i = 1; i < s.size(); i++){
+//             int curr = 1;
+//             if(s[i-1] == s[i]){
+//                 Map[i] += Map[i-1];
+//             }
+//             sum += Map[i];
+//         }
+//         return sum;
+//     }
+// };
+
+
+
+
+// APPROACH-2
+
+class Solution {
+public:
+    int countHomogenous(string s) {
+        int m=1e9+7;
+        int n=s.length();
+        int result=0;
+        int length=0;
+        for(int i=0;i<n;i++){
+            if(i>0 && s[i]==s[i-1]){
+                length+=1;
+            }
+            else{
+                length=1;
+            }
+            result=(result+length)%m;
+        }
+        return result;
+    }
+};
