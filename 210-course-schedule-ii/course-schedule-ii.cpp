2d63ce8ee08e6e695b8c177d9ef0c423a859d789
PIYUSH KUMAR <93852522+piyushkumar24@users.noreply.github.com>
2024-06-09 02:20:18 +0530

    Time: 16 ms (56.34%) | Memory: 16.7 MB (90.59%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<int>adj[n];
+        vector<int>indegree(n,0);
+        for(int i=0;i<pre.size();i++){
+            adj[pre[i][1]].push_back(pre[i][0]);
+            indegree[pre[i][0]]++;
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(topo.size()==n){
+            return topo;
+        }
+        return {};
+    }
+};
