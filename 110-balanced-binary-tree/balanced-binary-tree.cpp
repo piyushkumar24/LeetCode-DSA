3f753fe85a93135124fcf31b44ec85ffe576bc0c
PIYUSH KUMAR <93852522+piyushkumar24@users.noreply.github.com>
2024-11-26 21:23:03 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (8.49%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,20 +11,16 @@
  */
 class Solution {
 public:
-    bool isBalanced(TreeNode* root) {
-        return dfsHeight (root) != -1;
+    int dfsTree(TreeNode *root){
+        if(root==NULL) return 0;
+        int lh=dfsTree(root->left);
+        if(lh==-1) return -1;
+        int rh=dfsTree(root->right);
+        if(rh==-1) return -1;
+        if(abs(lh-rh)>1) return -1;
+        return max(lh,rh)+1;
     }
-
-    int dfsHeight (TreeNode *root) {
-        if (root == NULL) return 0;
-        int leftHeight = dfsHeight (root -> left);
-        if (leftHeight == -1) 
-            return -1;
-        int rightHeight = dfsHeight (root -> right);
-        if (rightHeight == -1) 
-            return -1;
-        if (abs(leftHeight - rightHeight) > 1)  
-            return -1;
-        return max (leftHeight, rightHeight) + 1;
+    bool isBalanced(TreeNode* root) {
+        return dfsTree(root)!=-1;
     }
 };
