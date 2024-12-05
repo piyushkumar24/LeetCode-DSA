9821d03c73b64d8adfd04e9c8a81e53c4704cde6
PIYUSH KUMAR <93852522+piyushkumar24@users.noreply.github.com>
2024-12-05 16:55:54 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (6.90%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -8,15 +8,9 @@ public:
         int low=1,high=n-2;
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-                return nums[mid];
-            }
-            if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1])){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
+            if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1])) low=mid+1;
+            else  high=mid-1;
         }
         return -1;
     }
