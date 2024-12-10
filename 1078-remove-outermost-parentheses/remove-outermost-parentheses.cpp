2b31461d7ad18ee7cbc5319468160e891328a6af
PIYUSH KUMAR <93852522+piyushkumar24@users.noreply.github.com>
2024-12-10 19:04:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (30.82%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -4,12 +4,8 @@ public:
         string res;
         int opened=0;
         for(int i=0;i<s.length();i++){
-            if(s[i]=='(' && opened++>0){
-                res+=s[i];
-            }
-            if(s[i]==')' && opened-->1){
-                res+=s[i];
-            }
+            if(s[i]=='(' && opened++>0) res+=s[i];
+            if(s[i]==')' && opened-->1) res+=s[i];
         }
         return res;
     }
