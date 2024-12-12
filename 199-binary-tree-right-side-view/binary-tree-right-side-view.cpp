39e8f8660dc38e6760250e6e5bcd762b03b11eca
PIYUSH KUMAR <93852522+piyushkumar24@users.noreply.github.com>
2024-12-13 01:05:11 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (31.12%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -13,10 +13,8 @@
 class Solution {
 public:
     void recursion(TreeNode* root,int level,vector<int> &ans){
-        if(root==NULL)
-        return;
-        if(ans.size()==level)
-        ans.push_back(root->val);
+        if(root==NULL) return;
+        if(ans.size()==level) ans.push_back(root->val);
         recursion(root->right,level+1,ans);
         recursion(root->left,level+1,ans);
     }
