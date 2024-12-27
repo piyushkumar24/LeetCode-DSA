ee65a1cb614445693b0ee4f63e690ded57815018
PIYUSH KUMAR <93852522+piyushkumar24@users.noreply.github.com>
2024-12-27 14:56:48 +0530

    Time: 4 ms (63.16%) | Memory: 27.4 MB (67.65%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -14,16 +14,15 @@ class Solution {
 public:
     map<int,int>mp; //declared outside function to remove tle
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        for(int i=0;i<inorder.size();i++)
+        for(int i=0;i<inorder.size();i++){
             mp[inorder[i]]=i;
+        }
         TreeNode* root=build(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1);
         return root;
     }
-
-    TreeNode* build(vector<int>& preorder,int prestart,int preend,vector<int>& inorder,int instart,int inend)
-    {
-        if(prestart>preend || instart>inend)    return NULL; //Terminating statement
-        TreeNode* root=new TreeNode(preorder[prestart]);
+    TreeNode *build(vector<int>&preorder,int prestart,int preend,vector<int>&inorder,int instart,int inend){
+        if(prestart>preend || instart>inend) return NULL; //Terminating statement
+        TreeNode *root=new TreeNode(preorder[prestart]);
         int rootpos=mp[preorder[prestart]]; //defined index of root alternatively mp[root->val];
         int leftnums=rootpos-instart; //defined numbers to the left of root
         root->left=build(preorder,prestart+1,prestart+leftnums,inorder,instart,rootpos-1);
