23e916bd0c44060138f0df59675831a968f04080
PIYUSH KUMAR <93852522+piyushkumar24@users.noreply.github.com>
2025-05-09 00:59:23 +0530

    Time: 35 ms (20.96%) | Memory: 24.5 MB (18.24%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,8 +1,7 @@
 class Solution {
-public:
 
-    void bfs(int row, int col, vector<vector<int>>&vis, vector<vector<char>>
-    &grid){
+private:
+    void bfs(int row, int col, vector<vector<int>>&vis, vector<vector<char>>&grid){
         vis[row][col]=1;
         int n=grid.size();
         int m=grid[0].size();
@@ -12,13 +11,15 @@ public:
             int row=q.front().first;
             int col=q.front().second;
             q.pop();
+            // Traverse the neibhours and mark them if it's a land
             for(int delrow=-1; delrow<=1; delrow++){
                 for(int delcol=-1; delcol<=1; delcol++){
                     int nrow=row+delrow;
                     int ncol=col+delcol;
-                    if (delrow != 0 && delcol != 0) continue;
-                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&
-                    !vis[nrow][ncol] && grid[nrow][ncol]=='1'){
+                    if(delrow!=0 && delcol!=0){
+                        continue;
+                    }
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1'&& !vis[nrow][ncol]){
                         vis[nrow][ncol]=1;
                         q.push({nrow, ncol});
                     }
@@ -27,6 +28,7 @@ public:
         }
     }
 
+public:
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
